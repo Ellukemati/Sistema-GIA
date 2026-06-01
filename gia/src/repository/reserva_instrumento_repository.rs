0734aed514dae9d856218c0c9b25a9f95a656a06
Francodlamas <francodlamas@outlook.com>
2026-05-31 22:04:47 -0300

    Implementacion inicial de reservas

---
+++ gia/src/repository/reserva_instrumento_repository.rs
@@ -0,0 +1,46 @@
+use crate::models::reserva_instrumento::ReservaInstrumento;
+use rusqlite::{Connection, Result as SqlResult};
+
+pub struct ReservaInstrumentoRepository;
+
+impl ReservaInstrumentoRepository {
+    pub fn crear(
+        conn: &Connection,
+        reserva_id: i64,
+        ejemplar_id: i64,
+    ) -> SqlResult<usize> {
+        conn.execute(
+            "INSERT INTO reserva_instrumentos
+            (reserva_id, ejemplar_id)
+            VALUES (?1, ?2)",
+            rusqlite::params![
+                reserva_id,
+                ejemplar_id
+            ],
+        )
+    }
+
+    pub fn obtener_por_reserva(
+        conn: &Connection,
+        reserva_id: i64,
+    ) -> SqlResult<Vec<ReservaInstrumento>> {
+        let mut stmt = conn.prepare(
+            "SELECT *
+             FROM reserva_instrumentos
+             WHERE reserva_id = ?1"
+        )?;
+
+        let filas = stmt.query_map(
+            [reserva_id],
+            ReservaInstrumento::from_row,
+        )?;
+
+        let mut relaciones = Vec::new();
+
+        for relacion in filas {
+            relaciones.push(relacion?);
+        }
+
+        Ok(relaciones)
+    }
+}
--- gia/src/routes/mod.rs
+++ gia/src/routes/mod.rs
@@ -1,23 +1,50 @@
 pub mod auth_routes;
 pub mod modelo_instrumento_routes;
 pub mod ejemplar_routes;
+pub mod reserva_routes;
 
 use rouille::{Request, Response};
-use rusqlite::Connection;
 use std::sync::{Arc, Mutex};
+use rusqlite::Connection;
 
-pub fn dispatch(request: &Request, conn: Arc<Mutex<Connection>>) -> Response {
-    let response = auth_routes::router(request, Arc::clone(&conn));
+pub fn dispatch(
+    request: &Request,
+    conn: Arc<Mutex<Connection>>,
+) -> Response {
+
+    let response =
+        auth_routes::router(request, Arc::clone(&conn));
+
     if response.status_code != 404 {
         return response;
     }
 
-    let response = modelo_instrumento_routes::router(request, Arc::clone(&conn));
+    let response =
+        modelo_instrumento_routes::router(
+            request,
+            Arc::clone(&conn),
+        );
+
     if response.status_code != 404 {
         return response;
     }
 
-    let response = ejemplar_routes::router(request, Arc::clone(&conn));
+    let response =
+        ejemplar_routes::router(
+            request,
+            Arc::clone(&conn),
+        );
+
+    if response.status_code != 404 {
+        return response;
+    }
+
+    let response =
+        reserva_routes::router(
+            request,
+            Arc::clone(&conn),
+        );
+
     if response.status_code != 404 {
         return response;
     }
