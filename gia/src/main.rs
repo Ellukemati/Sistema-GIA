8a8018a9b8c3721088adee27dcbce8b4787389d4
Geronimo Fanti <gfanti@fi.uba.ar>
2026-07-08 21:05:40 -0300

    f/Despliegue-Docker

--- gia/src/main.rs
+++ gia/src/main.rs
@@ -1,12 +1,17 @@
-use gia::{constants, db, server};
+use dotenvy::dotenv;
+use gia::{db, server};
+use std::env;
 
 fn main() {
-    let _conn = match db::init_db(constants::DB_PATH) {
+    dotenv().ok();
+
+    // Leer las variables (con un valor por defecto por si el .env falla
+    let db_path = env::var("DB_PATH").unwrap_or_else(|_| "data/gia.db".to_string());
+    let address = env::var("ADDRESS").unwrap_or_else(|_| "0.0.0.0:8080".to_string());
+
+    let _conn = match db::init_db(&db_path) {
         Ok(c) => {
-            println!(
-                "✓ Base de datos inicializada correctamente en: {}",
-                constants::DB_PATH
-            );
+            println!("✓ Base de datos inicializada correctamente en: {}", db_path);
             c
         }
         Err(e) => {
@@ -15,6 +20,6 @@ fn main() {
         }
     };
 
-    let server = server::Server::new(constants::ADDRESS, _conn);
+    let server = server::Server::new(&address, _conn);
     server.run();
 }
